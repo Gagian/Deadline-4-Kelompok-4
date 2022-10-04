8c1aa13e40917f6e080eeb55369d7ef52780267d
Gagian <90048383+Gagian@users.noreply.github.com>
2022-10-04 09:54:41 +0700

    Add files via upload

--- Deadline 3_Kelompok 4/LosikBarbershop/app/models.py
+++ Deadline 3_Kelompok 4/LosikBarbershop/app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class pelayan(models.Model):
-    idpelayan = models.CharField(max_length=3, primary_key=True)
+    idpelayan = models.AutoField(primary_key=True)
     namapelayan = models.CharField(max_length=50)
     notelp = models.IntegerField()
     alamat = models.CharField(max_length=50)
@@ -11,7 +11,7 @@ class pelayan(models.Model):
         return str(self.namapelayan)
 
 class layanan(models.Model):
-    idlayanan = models.CharField(max_length=3, primary_key=True)
+    idlayanan = models.AutoField(primary_key=True)
     jenislayanan = models.CharField(max_length=15)
     hargalayanan = models.IntegerField()
 
@@ -19,7 +19,7 @@ class layanan(models.Model):
         return str(self.jenislayanan)
 
 class transaksi(models.Model):
-    idtransaksi = models.CharField(max_length=3, primary_key=True)
+    idtransaksi = models.AutoField(primary_key=True)
     idpelayan = models.ForeignKey(pelayan,on_delete=models.CASCADE)
     namapelanggan = models.CharField(max_length=50)
     tanggaltransaksi = models.DateField()
@@ -28,7 +28,7 @@ class transaksi(models.Model):
         return str(self.namapelanggan)
 
 class detaillayanan(models.Model):
-    iddetaillayanan = models.CharField(max_length=3, primary_key=True)
+    iddetaillayanan = models.AutoField(primary_key=True)
     idtransaksi = models.ForeignKey(transaksi,on_delete=models.CASCADE)
     idlayanan = models.ForeignKey(layanan,on_delete=models.CASCADE)
 
